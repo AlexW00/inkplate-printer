725f1901b355550ae74b15364595ab38f23206c1
Alexander Weichart <>
2022-11-09 12:12:23 +0100

    formatted

--- clients/firmware/socket_controller.h
+++ clients/firmware/socket_controller.h
@@ -3,28 +3,30 @@
 #include <ArduinoJson.h>
 #include "socket.h"
 
-void send_example_message()
-{ 
+void send_example_message() {
   DynamicJsonDocument data(1024);
   data["message"] = "Hello from Arduino!";
   send_message("example", &data);
 }
 
-void send_message(String name, DynamicJsonDocument *data)
-{
-    DynamicJsonDocument doc(1024);
-    JsonArray arr = doc.to<JsonArray>();
+void send_message(String name, DynamicJsonDocument *data) {
+  DynamicJsonDocument doc(1024);
+  JsonArray arr = doc.to<JsonArray>();
 
-    arr.add(name);
-    if(data != nullptr) arr.add(*data);
+  arr.add(name);
+  if (data != nullptr) arr.add(*data);
 
-    String output;
-    serializeJson(doc, output);
-    socketIO.sendEVENT(output);
-    
-    Serial.println(output);
+  String output;
+  serializeJson(doc, output);
+  socketIO.sendEVENT(output);
+
+  Serial.println(output);
 }
 
-void socket_loop() { _socket_loop(); }
+void socket_loop() {
+  _socket_loop();
+}
 
-void socket_setup() { _socket_setup(); }
+void socket_setup() {
+  _socket_setup();
+}
